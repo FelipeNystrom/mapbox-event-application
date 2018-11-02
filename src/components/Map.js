6c13c0b775426eb8b4e26929f8d77ddb50e21360
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-11-02 06:11:46 +0100

    update dependencies

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import mapboxgl from 'mapbox-gl';
-import userSymbol from './userSymbol.svg';
-import FetchUserLocation from './FetchUserLocation';
+// import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+// import userSymbol from './Ripple-2.1s-200px.gif';
+// import FetchUserLocation from './FetchUserLocation';
 
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic2hha2thdGFsIiwiYSI6ImNqa2w4ZWIxbzAzcHQzcm1uenV3MG80OXIifQ.CY7fw2afzHzfcyCJzZvVhg';
@@ -26,18 +27,65 @@ class Map extends Component {
       center: [latitude, longitude]
     });
 
-    // Add zoom and rotation controls to the map.
-    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.on('move', () => {
+      const { lng, lat } = this.map.getCenter();
 
-    // Add geolocate control to the map.
-    this.map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        trackUserLocation: true
-      })
-    );
+      this.setState({
+        longitude: lng.toFixed(4),
+        latitude: lat.toFixed(4),
+        zoom: this.map.getZoom().toFixed(2)
+      });
+    });
+
+    this.map.on('load', function() {
+      this.map.loadImage('./Ripple-2.1s-200px.gif', (error, image) => {
+        if (error) throw error;
+        this.map.addImage('user', image);
+        this.map.addLayer({
+          id: 'points',
+          type: 'symbol',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [
+                {
+                  type: 'Feature',
+                  geometry: {
+                    type: 'Point',
+                    coordinates: [this.state.latitude, this.state.longitude]
+                  }
+                }
+              ]
+            }
+          },
+          layout: {
+            'icon-image': 'user',
+            'icon-size': 0.25
+          }
+        });
+      });
+    });
+
+    // this.map.addControl(
+    //   new MapboxGeocoder({
+    //     accessToken: mapboxgl.accessToken,
+    //     country: 'se'
+    //   })
+    // );
+
+    // // Add zoom and rotation controls to the map.
+    // this.map.addControl(new mapboxgl.NavigationControl());
+
+    // // Add geolocate control to the map.
+    // this.map.addControl(
+    //   new mapboxgl.GeolocateControl({
+    //     positionOptions: {
+    //       enableHighAccuracy: true
+    //     },
+    //     trackUserLocation: true
+    //   })
+    // );
 
     // Add user symbol at map load
     // this.map.on('load', () => {
